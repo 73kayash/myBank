13890d56456b359d8426a55a55a7fcb17af4b3db
Сергей Каяшов <sergey.kayashov@irlix.ru>
2022-05-26 12:25:01 +0400

    Развертка тестового приложения

---
+++ src/main/java/irlix/task/bank/Application.java
@@ -0,0 +1,11 @@
+package irlix.task.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
